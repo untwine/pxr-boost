154ee5758b06f904e83fd42b4cfa8476efa87346
Jeremy Retailleau <jeremy.retailleau@gmail.com>
2025-02-09 15:30:38 -0800

    Restructure the 'boost' library as a standalone package.
    - Isolate the 'boost' module from the OpenUSD repository;
    - Integrate a customized 'common.hpp.in' into the 'boost' library, with
      renamed namespace and version macros bound to the library;
    - Update include directives to use the new header prefix path;
    - Add identification and licensing information mandated by OpenUSD.

---
+++ src/pxr/boost/python/slice_nil.hpp
@@ -0,0 +1,62 @@
+//
+// Copyright 2024 Pixar
+// Licensed under the terms set forth in the LICENSE.txt file available at
+// https://openusd.org/license.
+//
+// Copyright David Abrahams 2002.
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+#ifndef PXR_EXTERNAL_BOOST_PYTHON_SLICE_NIL_HPP
+# define PXR_EXTERNAL_BOOST_PYTHON_SLICE_NIL_HPP
+
+#include <pxr/boost/python/common.hpp>
+
+# include <pxr/boost/python/detail/prefix.hpp>
+# include <pxr/boost/python/object_core.hpp>
+
+namespace PXR_BOOST_NAMESPACE { namespace python { namespace api {
+
+class slice_nil {};
+
+# ifndef _ // Watch out for GNU gettext users, who #define _(x)
+static const slice_nil _ = slice_nil();
+# endif 
+
+template <class T>
+struct slice_bound
+{
+    typedef object type;
+};
+
+template <>
+struct slice_bound<slice_nil>
+{
+    typedef slice_nil type;
+};
+
+}
+
+using api::slice_nil;
+# ifndef _ // Watch out for GNU gettext users, who #define _(x)
+using api::_;
+# endif 
+
+// Implement just enough of the Object Manager concept to allow slice_nil to
+// be converted to None.
+namespace converter
+{
+  template <>
+  struct object_manager_traits<api::slice_nil>
+  {
+      static constexpr bool is_specialized = true;
+  };
+}
+
+inline PyObject* get_managed_object(api::slice_nil const&, tag_t) {
+    return Py_None;
+}
+
+}} // namespace PXR_BOOST_NAMESPACE::python
+
+#endif // PXR_EXTERNAL_BOOST_PYTHON_SLICE_NIL_HPP
---
+++ test/upcast.cpp
@@ -0,0 +1,24 @@
+//
+// Copyright 2024 Pixar
+// Licensed under the terms set forth in the LICENSE.txt file available at
+// https://openusd.org/license.
+//
+// Copyright David Abrahams 2002.
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include <pxr/boost/python/cast.hpp>
+#include <cassert>
+
+struct X { long x; };
+struct Y : X, PyObject {};
+
+int main()
+{
+    PyTypeObject o;
+    Y y;
+    assert(PXR_BOOST_NAMESPACE::python::upcast<PyObject>(&o) == reinterpret_cast<PyObject*>(&o));
+    assert(PXR_BOOST_NAMESPACE::python::upcast<PyObject>(&y) == &y);
+    return 0;
+}
